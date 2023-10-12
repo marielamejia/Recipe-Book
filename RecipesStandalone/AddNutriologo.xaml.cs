25fb1861181e906a9dda515bd199237db66188d8
Antonio Rodríguez <antonio.rodriguez@itam.mx>
2023-10-12 15:07:13 -0600

    Se agregaron las ventanas para nutriologo

---
+++ RecipesStandalone/AddNutriologo.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RecipesStandalone
+{
+    /// <summary>
+    /// Lógica de interacción para AddNutriologo.xaml
+    /// </summary>
+    public partial class AddNutriologo : Window
+    {
+        public AddNutriologo()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void MenuAd_Click(object sender, RoutedEventArgs e)
+        {
+            Nutriologo w = new Nutriologo();
+            w.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ RecipesStandalone/BuscarNutriologo.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RecipesStandalone
+{
+    /// <summary>
+    /// Lógica de interacción para BuscarNutriologo.xaml
+    /// </summary>
+    public partial class BuscarNutriologo : Window
+    {
+        public BuscarNutriologo()
+        {
+            InitializeComponent();
+        }
+
+        private void RegresarBus_Click(object sender, RoutedEventArgs e)
+        {
+            Nutriologo w = new Nutriologo();
+            w.Show();
+            this.Hide();
+        }
+
+        private void BuscarBus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
